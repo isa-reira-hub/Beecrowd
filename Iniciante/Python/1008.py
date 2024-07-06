071072e3ea5fb87ccea77bb2d8969127e06e896f
Reiy <158104466+isa-reira-hub@users.noreply.github.com>
2024-07-06 18:19:44 -0300

    Create 1008.py

---
+++ Iniciante/Python/1008.py
@@ -0,0 +1,6 @@
+num_funcionario = int(input())
+num_hora = int(input())
+horas_recebidas = float(input())
+salario = num_hora * horas_recebidas
+print(f'NUMBER = {num_funcionario}')
+print(f'SALARY = U$ {salario:.2f}')
